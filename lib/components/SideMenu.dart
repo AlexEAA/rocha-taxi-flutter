8230119eeda8b86302b949be5a45fd454b7e2912
Aleks <45188136+AlexEAA@users.noreply.github.com>
2020-01-28 02:55:19 +0300

    Add drawer

--- lib/components/SideMenu.dart
+++ lib/components/SideMenu.dart
@@ -8,54 +8,48 @@ class SideMenu extends StatefulWidget {
 class _SideMenu extends State<SideMenu> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Text('Menu'),
-            decoration: BoxDecoration(
-              color: Colors.amber,
-            ),
-          ),
-          ListTile(
-            title: Text('Item 1'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Item 1'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Item 1'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Item 1'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
+  return Drawer(elevation: 16.0,
+  child: Column(
+    children: <Widget>[
+      UserAccountsDrawerHeader(
+        accountName: Text("Александр"),
+        accountEmail: Text("seoinetru@gmail.com"),
+        currentAccountPicture: CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Text("АЕ"),
+        ),
+        otherAccountsPictures: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Text("ae"),
+          )
         ],
       ),
-    );
+      ListTile(
+        title: new Text("Профиль"),
+        leading: new Icon(Icons.person),
+        onTap: () {Navigator.pushNamed(context, "/settings");},
+      ),
+     // Divider(
+     //   height: 0.1,
+     // ),
+      ListTile(
+        title: new Text("Настройки"),
+        leading: new Icon(Icons.settings),
+         onTap: () {Navigator.pushNamed(context, "/settings");},
+      ),
+      ListTile(
+        title: new Text("Оплата"),
+        leading: new Icon(Icons.payment),
+        onTap: () {Navigator.pushNamed(context, "/settings");},
+      ),
+      ListTile(
+        title: new Text("Еще"),
+        leading: new Icon(Icons.rate_review),
+        onTap: () {Navigator.pushNamed(context, "/settings");},
+      )
+    ],
+  ),
+  ); 
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:taxirocha/pages/MapPage/mapPage.dart';
 import 'package:taxirocha/util.dart';
 
